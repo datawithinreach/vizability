7902bb839e139fcec653f09795b27535e2c4973e
nhungbi <nhungvan16@gmail.com>
2024-02-21 19:44:14 -0500

    fned navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import HomePage from './pages/HomePage';
+import NavBar from './components/NavBar';
 
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <NavBar/>
           <Routes>
               <Route path="/VizAbility" element={<HomePage/>} />
           </Routes>
---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,24 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
+
+import ".././styles/NavBar.css"
+
+function NavBar() {
+  return (
+    <Navbar expand="lg" className='nav-bar' sticky='top'>
+      <Container>
+        <Navbar.Brand className='nav-brand' href="">VizAbility</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/VizAbility">Data Visualization</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
