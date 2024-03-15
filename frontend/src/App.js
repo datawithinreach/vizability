724b71683a707891bd3a39e5c76eb322ffbd25ce
nhungbi <nhungvan16@gmail.com>
2024-03-15 00:05:24 -0400

    set up firebase

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,18 @@ import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import HomePage from './pages/HomePage';
 import NavBar from './components/NavBar';
 
+import { SessionProvider } from './contexts/Session.jsx'
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <SessionProvider>
         <NavBar/>
           <Routes>
               <Route path="/VizAbility" element={<HomePage/>} />
           </Routes>
+          </SessionProvider>
         </Router>
     </div>
   );
